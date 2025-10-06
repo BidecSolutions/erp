eae0d8bbea4ed4961077d3d88cafb4afabb4d2ed
fatimajameel12 <fatima@bidecsol.com>
2025-10-06 02:01:31 -0700

    done

--- src/hrm/hrm_shift/shift.service.ts
+++ src/hrm/hrm_shift/shift.service.ts
@@ -13,27 +13,85 @@ export class ShiftService {
     private readonly shiftRepo: Repository<Shift>,
   ) {}
 
-  async create(dto: CreateShiftDto) {
-    const shift = this.shiftRepo.create(dto);
-    return await this.shiftRepo.save(shift);
-  }
+async create(dto: CreateShiftDto, company_id: number) {
+  try {
+    const shift = this.shiftRepo.create({
+      ...dto,
+      company_id, // direct assign company id
+    });
 
-  async findAll(filterStatus?: number) {
-    const status = filterStatus !== undefined ? filterStatus : 1;
-    return await this.shiftRepo.find({where: {status}});
+    await this.shiftRepo.save(shift);
+    const saved = await this.findAll(company_id);
+    return saved;
+  } catch (e) {
+    return { message: e.message };
   }
+}
 
-  async findOne(id: number) {
-    const shift = await this.shiftRepo.findOneBy({ id });
+
+  async findAll(company_id: number, filterStatus?: number) {
+  const status = filterStatus !== undefined ? filterStatus : 1;
+  try {
+    const shifts = await this.shiftRepo
+      .createQueryBuilder("shift")
+      .leftJoin("shift.company", "company")
+      .select([
+        "shift.id",
+        "shift.name",
+        "shift.status",
+        "company.company_name",
+      ])
+      .where("shift.company_id = :company_id", { company_id })
+      .andWhere("shift.status = :status", { status })
+      .orderBy("shift.id", "DESC")
+      .getRawMany();
+
+    return shifts;
+  } catch (e) {
+    return { message: e.message };
+  }
+}
+
+
+ async findOne(id: number) {
+  try {
+    const shift = await this.shiftRepo
+      .createQueryBuilder("shift")
+      .leftJoin("shift.company", "company")
+      .select([
+        "shift.id",
+        "shift.name",
+        "shift.status",
+        "company.company_name",
+      ])
+      .where("shift.id = :id", { id })
+      .getRawOne();
+
     if (!shift) throw new NotFoundException(`Shift ID ${id} not found`);
-    return shift;
-  }
 
-  async update(id: number, dto: UpdateShiftDto) {
-    const shift = await this.findOne(id);
-    Object.assign(shift, dto);
-    return await this.shiftRepo.save(shift);
+    return shift;
+  } catch (e) {
+    return { message: e.message };
   }
+}
+
+
+async update(id: number, dto: UpdateShiftDto, company_id: number) {
+  try {
+    const shift = await this.shiftRepo.findOne({ where: { id, company_id } });
+    if (!shift) throw new NotFoundException(`Shift ID ${id} not found`);
+
+    if (dto.name) shift.name = dto.name;
+
+    await this.shiftRepo.save(shift);
+
+    const updated = await this.findAll(company_id);
+    return updated;
+  } catch (e) {
+    return { message: e.message };
+  }
+}
+
 
   async remove(id: number) {
     const shift = await this.findOne(id);
