95de4e3a2b72dab1f62d3cd5046def7a2528d2e8
MUHAMMADMURAD63 <murad@bidecsol.com>
2025-10-23 15:52:30 +0500

    changes

--- src/app.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-
-@Controller()
-export class AppController { }
